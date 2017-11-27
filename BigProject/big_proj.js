1cc07180aaa30929fe270a369fc2b465bfe6368e
Phil Heroux <pherou2@uic.edu>
2017-11-27 15:31:05 +0000

    app shell cache working, indexed db working

--- BigProject/big_proj.js
+++ BigProject/big_proj.js
@@ -5,3 +5,76 @@
              .register('./sw_big_proj.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
+  
+  
+  
+  
+  //mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
+  
+  
+  var currentEvent = {};
+  
+  
+  var db2 = new Dexie('allEvents');
+
+	// Define a schema
+	db2.version(1).stores({
+		events: 'id, title, address, url'
+	});
+	
+	function saveFunction2(additem){
+    //currentEvent = saveitem;
+    //console.log("made it");
+    //console.log(saveitem);
+    db2.events.add({
+      id: additem.id,
+      title: additem.title,
+      address: additem.venue_address,
+      url: additem.url
+    });
+	}	
+	
+	
+  var db = new Dexie('savedEvents');
+
+	// Define a schema
+	db.version(1).stores({
+		events: 'id, title, address, url'
+	});
+
+
+	// Open the database
+	db.open().catch(function(error) {
+		alert('error : ' + error);
+	});
+	
+
+	function saveFunction(saveid){
+
+
+    var saveitem; 
+
+    db2.events.get({id: saveid}, function (item) {
+      saveitem={
+        "id": item.id,
+        "title": item.title,
+        "address": item.address,
+        "url": item.url
+      };
+      alert ("id: " + saveitem.id);
+      alert("title: "+ item.title);
+
+      console.log(saveitem);
+      
+      db.events.add({
+        id: saveitem.id,
+        title: saveitem.title,
+        address: saveitem.address,
+        url: saveitem.url
+      });
+
+      
+    });
+    
+
+	}
